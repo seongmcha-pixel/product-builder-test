15b6a14d523b7f3ff871829373919bfb1d7d0f9b
seongm.cha <seongm.cha@gmail.com>
2026-02-02 13:35:34 +0000

    Initial commit

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-// Add JS here
+const numbersContainer = document.querySelector('.numbers');
+const generateBtn = document.querySelector('#generate');
+
+const generateNumbers = () => {
+    numbersContainer.innerHTML = '';
+    const numbers = new Set();
+    while(numbers.size < 6) {
+        const randomNumber = Math.floor(Math.random() * 45) + 1;
+        numbers.add(randomNumber);
+    }
+
+    Array.from(numbers).forEach((number, index) => {
+        const numberEl = document.createElement('div');
+        numberEl.classList.add('number');
+        numberEl.textContent = number;
+        numberEl.style.animationDelay = `${index * 100}ms`;
+        numbersContainer.appendChild(numberEl);
+    });
+};
+
+generateBtn.addEventListener('click', generateNumbers);
+
+generateNumbers();
